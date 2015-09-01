574533a843cb30731d8a3e6de844826b127fd271
codename708 <codename708@gmail.com>
2015-09-01 23:18:34 +0900

    Initial Commit

---
+++ JobLine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JobLine
+//
+//  Created by naoya-kanoh on 2015/09/01.
+//  Copyright (c) 2015年 naoya.kanoh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
